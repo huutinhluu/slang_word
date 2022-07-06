145e903d73df0bdeb1d61f869a6ea1339d23e668
huutinhluu <huutinhluu@gmail.com>
2022-07-06 11:14:42 +0700

    add ChucNang2

--- Source/ConsoleAppSlangWord/src/consoleappslangword/ConsoleAppSlangWord.java
+++ Source/ConsoleAppSlangWord/src/consoleappslangword/ConsoleAppSlangWord.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
 /**
@@ -23,15 +24,17 @@ import java.util.regex.Pattern;
  */
 public class ConsoleAppSlangWord {
 	
-	public static  Map<String, List<String>> data;
+	public static  Map<String, List<String>> data_Root;
+	public static  Map<String, List<String>> data_Process;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
         
-        data = DocFile();
-        System.out.println( data );
+    	data_Root = DocFile();
+    	data_Process = DocFile_XuLy();
+        System.out.println( data_Process );
 
         
         
@@ -63,7 +66,9 @@ public class ConsoleAppSlangWord {
                   ChucNang1(keySlang);                  
                   break;
                 case 2:
-                  System.out.println("Chuc nang 2");
+                	System.out.print("Nhap definition :");
+                    String valueDefination = br.readLine();
+                    ChucNang2(valueDefination);
                   break;
                 case 3:
                   System.out.println("Chuc nang 3");
@@ -90,16 +95,18 @@ public class ConsoleAppSlangWord {
                   System.out.println("Chuc nang 10");
                   break;
                 default:
-                  System.out.println("Lua chon khong phu hop, vui long nhap lai");
+                	if(keyMenu != 0) {
+                		System.out.println("Lua chon khong phu hop, vui long nhap lai");
+                	}
               }
         } while(keyMenu != 0); 
     }
     
     public static void ChucNang1(String key){
     	key = key.toUpperCase();
-    	if(data.get(key)!= null ) {
+    	if(data_Process.get(key)!= null ) {
     		System.out.println("Cac definition cua " + key + " la: ");
-    		for(String item : data.get(key))
+    		for(String item : data_Root.get(key))
             {
           	  System.out.println("+ " + item);
             }
@@ -107,6 +114,20 @@ public class ConsoleAppSlangWord {
     	else {
     		System.out.println("Khong tim thay definition");
     	}
+    }
+    
+    public static void ChucNang2(String value){
+    	List<String> listSlang = getKey(value.toUpperCase());
+    	if(listSlang.size() > 0) {
+    		System.out.println("Cac slang word cua " + value + " la: ");
+    		for ( String item : listSlang) {
+    			System.out.println("+ " + item);
+    		}
+    	}
+    	else {
+    		System.out.println("Khong tim thay slang word nao");
+    	}
+    	
     }
     
     public static Map<String, List<String>> DocFile() throws FileNotFoundException, IOException{
@@ -127,6 +148,42 @@ public class ConsoleAppSlangWord {
 		}
         br.close();
         return data;
+    }
+    
+    public static Map<String, List<String>> DocFile_XuLy() throws FileNotFoundException, IOException{
+        String stringFile =""; 
+        Map<String, List<String>> data = new HashMap<String, List<String>>();
+        String str;
+                        BufferedReader br = new BufferedReader(new FileReader("slang.txt"));
+  		while (true)
+  		{
+  			str = br.readLine();
+                          if (str==null)
+  				break;
+                          List<String> listValue = new ArrayList<String>();                      
+                          String valueItem = str.split("`")[1].toUpperCase();
+                          valueItem = valueItem.replaceAll(Pattern.quote("|"), "-");
+                          listValue =  Arrays.asList(valueItem.split("-"));
+                          data.put(str.split("`")[0].toUpperCase(),listValue);                
+  		}
+          br.close();
+          return data;
+      }
+    
+    public static List<String> getKey(String string)
+    {
+    	List<String> listKey = new ArrayList<String>();
+        for (Entry<String, List<String>> entry: data_Process.entrySet())
+        {
+            List<String> listDefinition = entry.getValue();
+            for (String item : listDefinition) {
+            	if(item.contains(string))
+            	{
+            		listKey.add(entry.getKey());
+            	}
+            }
+        }
+        return listKey;
     }
     
 }
